725fab2e20d81ecf8ccc81f4e86fd94cdfcead6c
Progra-maria <78096795+Progra-maria@users.noreply.github.com>
2022-11-09 23:29:18 +0100

    Ajustada a 25 líneas. falta norminette

--- ft_split.c
+++ ft_split.c
@@ -1,4 +1,3 @@
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,88 +6,92 @@
 /*   By: maricarr <maricarr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 13:06:00 by maricarr          #+#    #+#             */
-/*   Updated: 2022/10/17 19:19:54 by maricarr         ###   ########.fr       */
+/*   Updated: 2022/11/09 23:27:59 by maricarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-static unsigned int ft_start(char *str, char c)
-{   
-    unsigned int    i;
-    
-    i = 0;
-    while (str[i] == c)
-            i++;
-    return (i);
+static unsigned int	ft_start(char *str, char c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] == c)
+	i++;
+	return (i);
 }
 
-static unsigned int    ft_end(char *str, char c)
+static unsigned int	ft_end(char *str, char c)
 {
-    unsigned int    i;
-    unsigned int    start;
+	unsigned int	i;
+	unsigned int	start;
 
-    i = 1;
-    start = ft_start(str, c);
-    while (str[start + i] != c)
-            i++;
-    return i;
+	i = 1;
+	start = ft_start(str, c);
+	while (str[start + i] != c && str[start + i] != 0)
+		i++;
+	return (i);
 }
 
 /*function that counts the total number of substrings that will be generated*/
-static long unsigned int ft_countstr(const char *str, char c)
+static long unsigned int	ft_countstr(const char *str, char c)
 {
-    unsigned long int   i;
-    unsigned long int   j;
-    char                *s;
+	unsigned long int	i;
+	unsigned long int	j;
+	char*			s;
 
-    i = 0;
-    j = 0;
-    s = (char *)str;
-    while (s[i])
-    {
-        if ((s[i] != c && s[i +1] == c) || (s[i] != c && s[i + 1] == '\0'))
-            j++;
-        i++;    
-    }
-    return (j);
+	i = 0;
+	j = 0;
+	s = (char *)str;
+	while (s[i])
+	{
+	if ((s[i] != c && s[i +1] == c) || (s[i] != c && s[i + 1] == '\0'))
+		j++;
+	i++;
+	}
+	return (j);
 }
 
-char **ft_split(char const *s, char c)
+char	*ft_strword(char *str, char c)
 {
-    int     words;
-    char    **newstring;
-    char    *string;
-    char    *end;
-    int     i;
+	char*	newstring;
+	newstring = ft_substr(str, ft_start(str, c), ft_end(str, c));
+	if (!newstring)
+	{
+		free(newstring);
+		return (0);
+	}
+	return (newstring);
+}
 
-    words = ft_countstr(s, c);
-    newstring = malloc((words + 1) * sizeof(char *));
-    if (!s || !newstring)
-        return (0);
-    newstring[words] = 0;
-    string = (char *)s;
-    i = 0;
-    words--;
-    while (words >= 0)
-    {
-    newstring[i] = ft_substr(string, ft_start(string, c), ft_end(string, c));
-    if (!newstring[i])
-        {
-        free(*newstring);
-        return (0);
-        }
-    string = &string[ft_start(string, c)];
-    end = &string[ft_end(string, c)];
-    words--;
-    i++;
-    while (*end == c)
-        end++;
-    if (end)
-        string = end;
-    }
-    return (newstring);
+char	**ft_split(char const *s, char c)
+{
+	int		words;
+	char**	newstring;
+	char*	string;
+	char*	end;
+	int		i;
+
+	words = ft_countstr(s, c);
+	newstring = malloc((words + 1) * sizeof(char *));
+	if (!s || !newstring)
+		return (0);
+	newstring[words] = 0;
+	string = (char *)s;
+	i = 0;
+	words--;
+	while (i <= words)
+	{
+	newstring[i] = ft_strword(string, c);
+	string = &string[ft_start(string, c)];
+	end = &string[ft_end(string, c)];
+		while (*end == c)
+			end++;
+		string = end;
+	i++;
+	}
+	return (newstring);
 }

0eb1c23205802bdab7a29a87604f28d3a0ade3de
PrograMaría <78096795+Progra-maria@users.noreply.github.com>
2023-01-16 22:09:11 +0100

    Create ft_lstlast_bonus.c
    Moulinette graded

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maricarr <maricarr@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 15:48:39 by maricarr          #+#    #+#             */
+/*   Updated: 2023/01/16 21:56:03 by maricarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+/**
+ * Ft_lstlast returns the last element of a linked list.
+ * 
+ * @param lst A pointer to the first link of a list.
+ * 
+ * @return The last element of the list.
+ */
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
